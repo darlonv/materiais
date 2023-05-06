54f41cf489af80a09c9f67ae50282ea818281911
darlonv <darlonv@gmail.com>
2023-05-06 11:54:28 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/darlonv/algoritmos.git
 git clone https://github.com/darlonv/redes.git
 git clone https://github.com/ifpr-cascavel/robotica.git
+git clone https://github.com/darlonv/so.git
 
 docker compose --profile init up 
 docker compose --profile init down 
