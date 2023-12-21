90bc2582c789d45d40f0c96e0b6912ab250f2465
Darlon <darlonv@gmail.com>
2023-12-21 10:58:26 -0300

    correções e bibliotecas adicionadas

--- src/components/Bib.jsx
+++ src/components/Bib.jsx
@@ -29,7 +29,70 @@ export default function Bib({ entrada, tipo = "citation", link = false }) {
           title  = {Algoritmos:  Teoria e Prática},
           publisher = {Grupo Gen},
           year   = {2022},
-        }`
+        }`,
+
+    'kurose2013_book': `
+        @book{kurose2013_book,
+          author  = {Jim Kurose, Keith W. Ross},
+          title  = {Redes de computadores e a internet: uma abordagem top-down},
+          publisher = {Pearson},
+          address   = {São Paulo},
+          year   = {2013},
+          number = {8},
+          isbn   = {9788581436777},
+          url    = {https://plataforma.bvirtual.com.br/Acervo/Publicacao/198909}
+        }`,
+    'torres2009_book': `
+        @book{torres2009_book,
+          author  = {Gabriel Torres},
+          title  = {Redes de Computadores},
+          publisher = {Novaterra},
+          address   = {Rio de Janeiro},
+          year   = {2009},
+          isbn   = {978-85-61893-05-7},
+          url    = {}
+        }`,
+    'stevens1994_book': `
+        @book{stevens1994_book,
+          author  = {W. Richard Stevens},
+          title  = {TCP/IP Illustrated: The Protocols},
+          volume = {1},
+          publisher = {Addison-Wesley/Pearson Education},
+          address   = {Boston, USA},
+          year   = {1994},
+          isbn   = {0-201-63346-9},
+          url    = {}
+        }`,
+    'forbellone2022_book': `
+        @book{forbellone2022_book,
+          author  = {A. L. V. Forbellone, H. G. Heberspätcher},
+          title  = {Lógica de programação: A construção de algoritmos e estruturas de dados},
+          publisher = {},
+          address   = {},
+          year   = {2022},
+          isbn   = {},
+          url    = {https://plataforma.bvirtual.com.br/Leitor/Publicacao/323/pdf/}
+        }`,
+    'tanenbaum2011_book': `
+        @book{tanenbaum2011_book,
+          author  = {A. S. Tanenbaum, D. Wetherall},
+          title  = {Redes de Computadores},
+          publisher = {Pearson do Brasil},
+          address   = {São Paulo},
+          year   = {2011},
+          isbn   = {},
+          url    = {https://plataforma.bvirtual.com.br/Acervo/Publicacao/2610}
+        }`,
+    'id': `
+        @book{id,
+          author  = {},
+          title  = {},
+          publisher = {},
+          address   = {},
+          year   = {},
+          isbn   = {},
+          url    = {}
+        }`,
   }
 
 
@@ -48,7 +111,7 @@ export default function Bib({ entrada, tipo = "citation", link = false }) {
   if (link) {
     //retorna a citação com o link para a url definida no bibtex.
     const bibFile = parseBibFile(bibContent[entrada]);
-    output = <a href={normalizeFieldValue(bibFile.getEntry(entrada).getField("url"))}>{output}</a>
+    output = <a href={normalizeFieldValue(bibFile.getEntry(entrada).getField("url"))} target="blank">{output}</a>
 
   }
 
