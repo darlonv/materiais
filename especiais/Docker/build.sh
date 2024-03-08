12632f10c9736bb3823acd5b423830760678647f
Darlon Vasata <darlonv@gmail.com>
2024-03-08 18:57:19 -0300

    update

--- especiais/Docker/build.sh
+++ especiais/Docker/build.sh
@@ -8,13 +8,12 @@ then
     docker build -t darlon/quarto:latest -f Dockerfile-quarto-linux .
 fi
 
-if [ ${ARCH} == 'aarch64' ];
+if [ ${ARCH} == 'arm64' ];
 then
     echo ${ARCH}:
     docker build -t darlon/quarto:latest -f Dockerfile-quarto-osx .
 fi
 
-# docker build -t darlon/quarto:latest -f Dockerfile-quarto .
 docker build -t darlon/quarto-full:latest -f Dockerfile-quarto-full .
 
 docker push darlon/quarto:latest
