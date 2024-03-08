506e862a1bf742583e4f8abc479743d26b8b952d
Darlon <darlonv@gmail.com>
2024-03-08 18:49:37 -0300

    dockerfile diferente para osx e linux

--- especiais/Docker/build.sh
+++ especiais/Docker/build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-docker build -t darlon/quarto:latest -f Dockerfile-quarto .
+ARCH=$(uname -m)
+
+if [ ${ARCH} == 'x86_64' ];
+then
+    echo ${ARCH}:
+    docker build -t darlon/quarto:latest -f Dockerfile-quarto-linux .
+fi
+
+if [ ${ARCH} == 'aarch64' ];
+then
+    echo ${ARCH}:
+    docker build -t darlon/quarto:latest -f Dockerfile-quarto-osx .
+fi
+
+# docker build -t darlon/quarto:latest -f Dockerfile-quarto .
 docker build -t darlon/quarto-full:latest -f Dockerfile-quarto-full .
+
 docker push darlon/quarto:latest
 docker push darlon/quarto-full:latest
