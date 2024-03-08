b367e4559ca38942347cde5f2a7bef22392c4abf
Darlon Vasata <darlonv@gmail.com>
2024-03-08 18:10:32 -0300

    update

--- especiais/Docker/build.sh
+++ especiais/Docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t darlon/quarto:latest -f Dockerfile-quarto .
-# docker build -t darlon/quarto-full:latest -f Dockerfile-quarto-full .
-# docker push darlon/quarto:latest
-# docker push darlon/quarto-full:latest
+docker build -t darlon/quarto-full:latest -f Dockerfile-quarto-full .
+docker push darlon/quarto:latest
+docker push darlon/quarto-full:latest
