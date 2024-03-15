cd306902526a0df5ead32059568a0f17d5f353f2
Darlon <darlonv@gmail.com>
2024-03-14 23:21:39 -0300

    arquivos com nome da arquitetura

--- especiais/Docker/build.sh
+++ especiais/Docker/build.sh
@@ -7,8 +7,8 @@ then
     PLATFORM="linux/amd64"
     ARCHNAME="-amd64"
     echo ${ARCH}:
-    docker build -t darlon/quarto${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-linux .
-    docker build -t darlon/quarto-full${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-full .
+    docker build -t darlon/quarto${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-amd64 .
+    docker build -t darlon/quarto-full${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-full-amd64 .
 
     docker push darlon/quarto${ARCHNAME}:latest 
     docker push darlon/quarto-full${ARCHNAME}:latest
@@ -20,17 +20,17 @@ then
     PLATFORM="linux/arm64"
     ARCHNAME="-arm64"
     echo ${ARCH}:
-    docker build -t darlon/quarto${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-osx .
-    docker build -t darlon/quarto-full${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-full .
+    docker build -t darlon/quarto${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-arm64 .
+    docker build -t darlon/quarto-full${ARCHNAME}:latest --platform ${PLATFORM} -f Dockerfile-quarto-arm64 .
 
     docker push darlon/quarto${ARCHNAME}:latest 
     docker push darlon/quarto-full${ARCHNAME}:latest
 fi
 
 
-#docker manifest create darlon/quarto darlon/quarto-amd64:latest darlon/quarto-arm64:latest
-#docker manifest push darlon/quarto
+docker manifest create darlon/quarto darlon/quarto-amd64:latest darlon/quarto-arm64:latest
+docker manifest push darlon/quarto
 
-#docker manifest create darlon/quarto-full darlon/quarto-full-amd64:latest darlon/quarto-full-arm64:latest
-#docker manifest push darlon/quarto-full
+docker manifest create darlon/quarto-full darlon/quarto-full-amd64:latest darlon/quarto-full-arm64:latest
+docker manifest push darlon/quarto-full
 
