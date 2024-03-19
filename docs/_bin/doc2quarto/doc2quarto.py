aa3e7ce26239e26eba7309d23229320c81eea78a
Darlon <darlonv@gmail.com>
2024-03-18 23:49:51 -0300

    conversor md2qmd

--- docs/_bin/doc2quarto/doc2quarto.py
+++ docs/_bin/doc2quarto/doc2quarto.py
@@ -1,4 +1,7 @@
+
+
 import re
+import sys
 
 
 def remover(content):
@@ -30,7 +33,14 @@ def replace(content):
         '<TabItem value="python" label="c">', '\n::: {.content-visible when-profile="c"}\n')
     content = content.replace("</TabItem>", "\n:::\n")
     content = content.replace("```mermaid", "```{mermaid}")
+    content = content.replace(
+        "<!-- --- -->", '\n\n::: {.content-visible when-profile="slides"}\n---\n:::\n\n')
 
+    return content
+
+
+def fillTitle(content, title):
+    content = content.replace("<!-- title -->", title)
     return content
 
 
@@ -40,6 +50,22 @@ def convertDoc2Quarto(content):
     # print(converting)
 
     return content
+
+
+def insertHeader(header, content):
+    content = header + '\n\n' + content
+    return content
+
+
+def getTitle(content):
+    title = ''
+
+    content = content.lstrip()
+    pos_br = content.find('\n')
+    pos_space = content.find(' ')
+    title = content[pos_space+1:pos_br]
+
+    return title
 
 
 def loadFile(filename):
@@ -55,10 +81,29 @@ def saveFile(filename, content):
         file.write(content)
 
 
-if __name__ == "__main__":
-    filename_in = "./02-Selecao.md"
-    filename_out = "./02-Selecao.qmd"
+def startMd2Qmd(filename_in, filename_header="./quarto_pdf_header.txt"):
+    # filename_in = "./02-Selecao.md"
+    filename_out = filename_in + ".qmd"  # "./02-Selecao.qmd"
+    # filename_header = "./quarto_pdf_header.txt"
+
     content = loadFile(filename_in)
     # print(content)
     content = convertDoc2Quarto(content)
+    header = loadFile(filename_header)
+    title = getTitle(content)
+    # print(len(title), title)
+    header = fillTitle(header, title)
+    content = insertHeader(header, content)
     saveFile(filename_out, content)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Nome de arquivo .md faltando.")
+        exit(1)
+
+    for i in range(1, len(sys.argv)):
+        filename = sys.argv[i]
+        print(filename, "..", end=' ')
+        startMd2Qmd(filename)
+        print("done.")
